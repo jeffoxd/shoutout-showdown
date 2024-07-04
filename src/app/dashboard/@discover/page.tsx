3b3cee7db68ef6dfac6ef9356eb1634d0be9b3ce
Jeff Ng <50jeffng@gmail.com>
2024-07-04 09:16:49 +0800

    fix: still playing bug when modal closes

--- src/app/dashboard/@discover/page.tsx
+++ src/app/dashboard/@discover/page.tsx
@@ -5,27 +5,30 @@ import Card from "@/components/Card";
 export default function Page() {
   const cardWidth = 360;
   const heightWidth = 240;
+  const closeModal = () => {
+    var video = document.getElementById("vid");
+    //@ts-ignore
+    document.getElementById("vid").src = "";
+  };
   return (
     <div className="grid grid-cols-3 gap-y-8">
       <Card
         width={cardWidth}
         height={heightWidth}
         src="/images/education_website_presentation_thumbnail.png"
-        onClick={() =>
+        onClick={() => {
           //@ts-ignore
-          document.getElementById("educational_video_modal").showModal()
-        }
+          document.getElementById("vid").src =
+            "https://drive.google.com/file/d/1847AuP2c0UWaUVaBlhKZ1uzr1wzx0Fn-/preview";
+          //@ts-ignore
+          document.getElementById("educational_video_modal").showModal();
+        }}
       />
       <dialog id="educational_video_modal" className="modal">
         <div className="modal-box max-w-full h-5/6">
-          <iframe
-            src="https://drive.google.com/file/d/1847AuP2c0UWaUVaBlhKZ1uzr1wzx0Fn-/preview"
-            width="100%"
-            height="100%"
-            allow="autoplay"
-          ></iframe>
+          <iframe id="vid" width="100%" height="100%" allow="autoplay"></iframe>
         </div>
-        <form method="dialog" className="modal-backdrop">
+        <form method="dialog" className="modal-backdrop" onClick={closeModal}>
           <button>close</button>
         </form>
       </dialog>
